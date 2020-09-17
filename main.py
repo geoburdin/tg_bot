ecfec062086dbeefdd9d1b327a5418bc27b3bd1d
geoburdin <62799944+geoburdin@users.noreply.github.com>
2020-09-17 18:08:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -33,12 +33,10 @@ def check(url):
         XPATH_AVAILABILITY = '//div[@id ="availability"]//text()'
         RAw_AVAILABILITY = doc.xpath(XPATH_AVAILABILITY)
         AVAILABILITY = ''.join(RAw_AVAILABILITY).strip() if RAw_AVAILABILITY else None
-        if AVAILABILITY == 'No disponible.' or 'Disponible a través de estos vendedores.':
+        if AVAILABILITY == 'No disponible.' or AVAILABILITY == 'Disponible a través de estos vendedores.' or len(
+                AVAILABILITY) > 100:
             AVAILABILITY = None
         return AVAILABILITY
-
-
-items=[]
 
 
 @bot.message_handler(commands=["hello"])
@@ -56,6 +54,7 @@ def add_item(message):
 def add(message):
     items.append(message.text)
 
+
 import threading
 @bot.message_handler(commands=["start"])
 def start(message):
@@ -68,13 +67,16 @@ def handle(message):
     bot.send_message(id_chat, "Ok, I`m looking at items every 10 min", reply_markup=keyboard1)
 
     while True:
+
         time.sleep(600)
 
         for item in items:
 
+
             if check(item) != None :
                 bot.reply_to(message, "I have something for you: \n" + item, reply_markup=keyboard1)
                 items.remove(item)
+
 
 
 
@@ -86,20 +88,40 @@ def delete_item(message):
 
 
 def delete(message):
-    items.remove(message.text)
+    if message.text in items:
+        items.remove(message.text)
+
+    else:
+        bot.reply_to(message, 'There`s not such link in the list')
 
 
 @bot.message_handler( commands= ["look_at_my_list"])
 def show_list(message):
     id_chat = message.chat.id
+    list = open('list.txt', 'w')
     for item in items:
         msg = bot.reply_to(message, str(item))
-    bot.reply_to(message, 'That`s all from the list')
+        list.write(item + '\n')
+    list.close()
+    bot.reply_to(message, 'That`s all from the list \n I`ve save the list in a file')
+    f = open("list.txt", "rb")
+    bot.send_document(message.chat.id, f)
+    f.close()
 
+
+
+
+items=[]
 while True:
     try:
+        lists = open('list.txt')
+        list = lists.read().splitlines()
+        for item in list:
+            items.append(item)
+        print(items)
+        lists.close()
         bot.polling()
 
     except Exception as e:
-
+        print(e)
         time.sleep(60)
